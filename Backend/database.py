6cabcd591dfed31dca9510618117a8dcdf4e4617
david <D00239311@student.dkit.ie>
2023-10-28 13:41:46 +0100

    :seedling: Added relevant collections seen in db

--- Backend/database.py
+++ Backend/database.py
@@ -9,6 +9,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Initialize the MongoDB client
 client = MongoClient(os.getenv('MONGO_URI'), server_api=ServerApi('1'))
 db = client[os.getenv('MONGO_DB_NAME')]
-collection = db['your_collection_name']
+
+# Create collections
+users_collection = db['users']
+routes_collection = db['routes']
+waypoints_collection = db['waypoints']
+stations_collection = db['stations']
+notifications_collection = db['notifications']
+discussions_collection = db['discussions']
+fuel_cards_collection = db['fuel_cards']
+temp_nn_collection = db['nn_storage']
