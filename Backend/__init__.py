9dfbc03c23da3c7f71ef51615d6baa21f0eacf93
ndavido <ndavido02@gmail.com>
2023-10-28 20:57:24 +0100

    🎨 Improved Register & Verify Code

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 import os
-from flask import Flask, request, jsonify, sessions
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
-from database import db, temp_nn_collection, users_collection
+from database import Database, UserCollection
 import bcrypt
 from twilio.rest import Client
 import random
@@ -12,11 +12,16 @@ from datetime import datetime
 load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:19006"}})
+app.secret_key = os.urandom(24)
+
 
 account_sid = os.getenv('TWILIO_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 twilio_number = os.getenv('TWILIO_PHONE_NUMBER')
 twilio_client = Client(account_sid, auth_token)
+
+db = Database()
+users_collection = UserCollection(db)
 
 tasks = []
 users = {}
@@ -24,64 +29,74 @@ users = {}
 
 @app.route('/register', methods=['POST'])
 def register():
-    # Get JSON data from the request
-    data = request.get_json()
-    full_name = data.get('full_name')
-    username = data.get('username')
-    phone_number = data.get('phone_number')
-    phone_number = "+" + phone_number
+    try:
+        data = request.get_json()
+        full_name = data.get('full_name')
+        username = data.get('username')
+        phone_number = data.get('phone_number')
+        phone_number = "+" + phone_number
 
-    # Check if user already exists
-    if username in users:
-        return jsonify({"error": "Username already exists"}), 409
+        # Check if user already exists in the database
+        existing_user = users_collection.find_user({"username": username})
+        if existing_user:
+            return jsonify({"error": "Username already exists"}), 409
 
-    # Generate a 6-digit verification code
-    verification_code = str(random.randint(100000, 999999))
+        verification_code = str(random.randint(100000, 999999))
+        hashed_code = bcrypt.hashpw(
+            verification_code.encode('utf-8'), bcrypt.gensalt())
 
-    # Hash the verification code using bcrypt for secure storage
-    hashed_code = bcrypt.hashpw(verification_code.encode('utf-8'), bcrypt.gensalt())
+        # Temporarily store user data in the session
+        session['temp_user'] = {
+            "full_name": full_name,
+            "username": username,
+            "phone_number": phone_number,
+            "verification_code": hashed_code,
+            "verified": False,
+            "login_code": "",
+            "roles": ["user"],
+            "created_at": datetime.now(),
+            "updated_at": datetime.now()
+        }
 
-    # Store user data in the users dictionary
-    users[username] = {
-        "full_name": full_name,
-        "username": username,
-        "phone_number": phone_number,
-        "verification_code": hashed_code,
-        "verified": False,
-        "login_code": "",
-        "roles": ["user"],
-        "created_at": datetime.now(),
-        "updated_at": datetime.now()
-    }
+        # Send verification code via SMS using Twilio
+        message = twilio_client.messages.create(
+            to=phone_number,
+            from_=twilio_number,
+            body=f"Your verification code is: {verification_code}"
+        )
 
-    # Send the verification code via SMS using Twilio
-    message = twilio_client.messages.create(
-        to=phone_number,
-        from_=twilio_number,
-        body=f"Your verification code is: {verification_code}"
-    )
+        return jsonify({"message": "Verification code sent successfully!"})
 
-    return jsonify({"message": "Verification code sent successfully!"})
+    except Exception as e:
+        # Log the exception for debugging
+        print(f"Error: {str(e)}")
+        return jsonify({"error": "An error occurred while processing your request."}), 500
 
 
 @app.route('/register/verify', methods=['POST'])
 def verify():
-    # Get JSON data from the request
-    data = request.get_json()
-    username = data.get('username')
-    code = data.get('code')
+    try:
+        data = request.get_json()
+        username = data.get('username')
+        code = data.get('code')
 
-    # Retrieve the user from the users dictionary
-    user = users.get(username)
+        temp_user = session.get('temp_user')
+        if not temp_user or temp_user['username'] != username:
+            return jsonify({"error": "User data not found or session expired"}), 404
 
-    # Check if the user exists and if the verification code matches
-    if user and bcrypt.checkpw(code.encode('utf-8'), user['verification_code']):
-        # this inserts the users into the collection, the also takes them out of the dictionary
-        users_collection.insert_one(user)
-        users.pop(username)
-        return jsonify({"message": "Verification successful!"})
-    else:
+        if bcrypt.checkpw(code.encode('utf-8'), temp_user['verification_code']):
+            # Add the user's data to the database
+            users_collection.insert_user(temp_user)
+            # Clear the temporary data from the session
+            session.pop('temp_user', None)
+            return jsonify({"message": "Verification successful!"})
+
         return jsonify({"error": "Verification failed"}), 401
+
+    except Exception as e:
+        # Log the exception for debugging
+        print(f"Error: {str(e)}")
+        return jsonify({"error": "An error occurred while verifying your code."}), 500
 
 
 @app.route('/login', methods=['POST'])
@@ -95,11 +110,13 @@ def login():
     login_code = str(random.randint(100000, 999999))
 
     # Hash the login code using bcrypt for secure storage
-    hashed_login_code = bcrypt.hashpw(login_code.encode('utf-8'), bcrypt.gensalt())
+    hashed_login_code = bcrypt.hashpw(
+        login_code.encode('utf-8'), bcrypt.gensalt())
 
     # Store the login code in the user's data
     # Check if user exists by phone number
-    user = next((u for u in users.values() if u["phone_number"] == phone_number), None)
+    user = next((u for u in users.values()
+                if u["phone_number"] == phone_number), None)
     if user:
         user["login_code"] = hashed_login_code
     else:
@@ -124,7 +141,8 @@ def login_verify():
     code = data.get('code')
 
     # Retrieve the user by phone number
-    user = next((u for u in users.values() if u["phone_number"] == phone_number), None)
+    user = next((u for u in users.values()
+                if u["phone_number"] == phone_number), None)
 
     # Check if the user exists and if the login code matches
     if user and bcrypt.checkpw(code.encode('utf-8'), user['login_code']):
@@ -133,24 +151,24 @@ def login_verify():
     return jsonify({"error": "Login failed"}), 401
 
 
-## Neural Network Connection
-@app.route('/store_fuel_prices', methods=['POST'])
-def store_fuel_prices():
-    try:
-        data = request.get_json()
+# Neural Network Connection
+# @app.route('/store_fuel_prices', methods=['POST'])
+# def store_fuel_prices():
+#     try:
+#         data = request.get_json()
 
-        date = data.get('date')
-        price = data.get('price')
+#         date = data.get('date')
+#         price = data.get('price')
 
-        fuel_price_data = {
-            "date": date,
-            "price": price
-        }
-        temp_nn_collection.insert_one(fuel_price_data)
+#         fuel_price_data = {
+#             "date": date,
+#             "price": price
+#         }
+#         temp_nn_collection.insert_one(fuel_price_data)
 
-        return jsonify({"message": "Fuel price inserted"})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+#         return jsonify({"message": "Fuel price inserted"})
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
