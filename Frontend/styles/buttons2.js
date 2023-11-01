8cbb973995707a636616734d31a31e38fcafc519
David <D00238516@student.dkit.ie>
2023-11-01 03:15:09 +0000

    🔨 Changes To the Login, Register & Welcome Screens

---
+++ Frontend/styles/buttons2.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const ButtonContainer2 = styled.TouchableOpacity`
+  margin-bottom: 10px;
+  padding: 12px;
+  width: 50%;
+  float: left;
+  border-radius: 10px;
+  background-color: ${props => props.bgColor};
+`;
+
+const ButtonText2 = styled.Text`
+  font-size: 16px;
+  text-align: center;
+  color: white;
+  color: ${props => props.txtColor};
+`;
+
+const PressableButton2 = ({ onPress, bgColor, txtColor, title }) => (
+  <ButtonContainer2 onPress={onPress} bgColor={bgColor}>
+    <ButtonText2 txtColor={txtColor}>{title}</ButtonText2>
+  </ButtonContainer2>
+);
+export default PressableButton2;
