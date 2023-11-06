9e66d6b77754d6e1e4397646713bcba9d89ea28c
DavidCampion <campiondavid02@gmail.com>
2023-11-06 11:12:26 +0000

    📝 Fixed Issue with Login Screen

--- Frontend/Screens/LoginScreen.js
+++ Frontend/Screens/LoginScreen.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import axios from 'axios';
 
 // Styled Components and other imports
-import { Main, ContainerWrapper, ContainerInner, ContainerContent, BttnDiv, TxtWrapper, WelcomeTxt, BttnDiv2, InputWrapper, InputTxt } from '../styles/wrapper';
+import {
+    Main,
+    ContainerWrapper,
+    ContainerInner,
+    ContainerContent,
+    BttnDiv,
+    TxtWrapper,
+    WelcomeTxt,
+    BttnDiv2,
+    InputWrapper,
+    InputTxt
+} from '../styles/wrapper';
 import PressableButton from '../styles/buttons';
 import PressableButton2 from '../styles/buttons2';
 import Logo from '../styles/logo';
@@ -13,80 +24,99 @@ import Logo from '../styles/logo';
 import CountryAndNumberInput from '../styles/countryCodePlusNum';
 
 const LoginScreen = () => {
-  const navigation = useNavigation();
-  const [formData, setFormData] = useState({
-    phone_number: '',
-  });
+    const navigation = useNavigation();
+    const [formData, setFormData] = useState({
+        phone_number: '',
+    });
 
-  const countryCode = '353';
+    const countryCode = '353';
 
-  const [message, setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
-  const handleChange = (name, value) => {
-    setFormData({ ...formData, [name]: value });
-  };
+    const handleChange = (name, value) => {
+        setFormData({...formData, [name]: value});
+    };
 
-  const handleLogin = async () => {
-    try {
-      const apiKey = process.env.REACT_NATIVE_API_KEY;
+    const handleLogin = async () => {
+        try {
+            const apiKey = process.env.REACT_NATIVE_API_KEY;
 
-      const fullNum = `${countryCode}${formData.phone_number}`;
+            const fullNum = `${countryCode}${formData.phone_number}`;
 
-      // Add the API key to the request headers
-      const config = {
-        headers: {
-          'X-API-Key': apiKey,
-        },
-      };
+            // Add the API key to the request headers
+            const config = {
+                headers: {
+                    'X-API-Key': apiKey,
+                },
+            };
 
-      console.log(config);
+            console.log(config);
 
-      const response = await axios.post('http://127.0.0.1:5000/login', { ...formData, phone_number: fullNum }, config);
-      if (response && response.data) {
-        setMessage(response.data.message);
+            const response = await axios.post('http://127.0.0.1:5000/login', {
+                ...formData,
+                phone_number: fullNum
+            }, config);
+            if (response && response.data) {
+                setMessage(response.data.message);
 
-        if (response.data.message === 'Login code sent successfully!') {
-          navigation.navigate('LoginVerify', { phone: fullNum }); // Navigate to code verification screen
+                if (response.data.message === 'Login code sent successfully!') {
+                    navigation.navigate('LoginVerify', {phone: fullNum}); // Navigate to code verification screen
+                }
+            } else {
+                // Handle other cases or errors if needed
+            }
+        } catch (error) {
+            setMessage(error.response?.data?.error || 'An error occurred.');
         }
-      } else {
-        // Handle other cases or errors if needed
-      }
-    } catch (error) {
-      setMessage(error.response?.data?.error || 'An error occurred.');
-    }
-  };
+    };
 
-  return (
-    <Main>
-      <Logo />
-      <ContainerWrapper>
-        <ContainerInner>
-          <ContainerContent>
-            <BttnDiv2>
-              <PressableButton2
-                onPress={() => navigation.navigate('Register')}
-                title="Register"
-                bgColor="#6bff91"
-                txtColor="white"
-              />
-              <PressableButton2 title="Login" bgColor="white" txtColor="black" />
-            </BttnDiv2>
-            <InputWrapper>
-              <Text>Country Code</Text>
-              <InputTxt value="353" editable={false} placeholder="" />
-              <Text>Phone Number</Text>
-              {/* Replace the existing phone number input with CountryAndNumberInput */}
-              <CountryAndNumberInput value={formData.phone_number} onChangeText={(text) => handleChange('phone_number', text)} />
-              <Text>{message}</Text>
-            </InputWrapper>
-            <BttnDiv>
-              <PressableButton onPress={handleLogin} title="Send Login Code" bgColor="#6bff91" />
-            </BttnDiv>
-          </ContainerContent>
-        </ContainerInner>
-      </ContainerWrapper>
-    </Main>
-  );
+    return (
+        <Main>
+            <Logo/>
+            <ContainerWrapper>
+                <ContainerInner>
+                    <ContainerContent>
+                        <BttnDiv2>
+                            <PressableButton2
+                                onPress={() => navigation.navigate('Register')}
+                                title='Register'
+                                bgColor='#6bff91'
+                                txtColor='white'
+                            />
+                            <PressableButton2
+
+                                title='Login'
+                                bgColor='white'
+                                txtColor='black'
+                            />
+                        </BttnDiv2>
+                        <InputWrapper>
+                            <Text>Country Code</Text>
+                            <InputTxt
+                                value="353"
+                                editable={false}
+                                placeholder=""
+                                onChangeText={(text) => handleChange('country_code', text)}
+                            />
+                            <Text>Phone Number</Text>
+                            <InputTxt
+                                placeholder=""
+                                onChangeText={(text) => handleChange('phone_number', text)}
+                            />
+                            <Text>{message}</Text>
+                        </InputWrapper>
+                        <BttnDiv>
+                            <PressableButton
+                                onPress={handleLogin}
+                                title='Send Login Code'
+                                bgColor='#6bff91'
+                            />
+                        </BttnDiv>
+                    </ContainerContent>
+                </ContainerInner>
+            </ContainerWrapper>
+        </Main>
+    );
 };
 
 export default LoginScreen;
