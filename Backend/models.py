e39cca1d020917ff0d9746bb8c93447d6185b544
david <D00239311@student.dkit.ie>
2023-12-10 21:21:21 +0000

    :sparkles: updated to account for EV vehicles, charge points, rates

--- Backend/models.py
+++ Backend/models.py
@@ -1,7 +1,6 @@
 # models.py
 
-from mongoengine import Document, StringField, FloatField, IntField, ReferenceField, BooleanField, ListField, \
-    DateTimeField
+from mongoengine import Document, StringField, FloatField, IntField, ReferenceField, BooleanField, ListField, DateTimeField
 
 class Location(Document):
     latitude = FloatField(required=True)
@@ -19,6 +18,7 @@ class Users(Document):
     roles = ListField(StringField())
     created_at = DateTimeField()
     updated_at = DateTimeField()
+
 class Vehicle(Document):
     user = ReferenceField(Users, reverse_delete_rule='CASCADE')
     year = IntField()
@@ -26,18 +26,25 @@ class Vehicle(Document):
     model = StringField()
     engine_size = StringField()
     license_plate = StringField()
-    fuel_type = StringField()
+    fuel_type = StringField(choices=('Petrol', 'Diesel', 'Electric'))  # Include 'Electric' as an option
 
 class FuelStation(Document):
     name = StringField(required=True)
+    location = ReferenceField(Location, required=True)
+    is_charging_station = BooleanField(default=False)
+    charging_rates = FloatField()  # Price per kWh if this is a charging station
 
 class FuelPrices(Document):
     fuel_station = ReferenceField(FuelStation, reverse_delete_rule='CASCADE')
-    price = StringField()
+    petrol_price = FloatField()
+    diesel_price = FloatField()
+    electricity_price = FloatField()  # Price per kWh
+    updated_at = DateTimeField(required=True)
 
 class UserFuelBudget(Document):
-    user = ReferenceField('Users', required=True)  # Assuming you have a Users model for user information
-    weekly_fuel_budget = FloatField(required=True)  # The user's fuel budget for the week
-    location = ReferenceField(Location, required=True)  # The user's location
-    busy_pump = BooleanField(required=True)  # User input: whether the pump is busy or not
-    high_price = BooleanField(required=True)  # User input: whether the price is considered high
+    user = ReferenceField('Users', required=True)
+    weekly_budget = FloatField(required=True)  # Will need to update these fields as we go with encopassing EV
+    location = ReferenceField(Location, required=True)
+    busy_pump = BooleanField(required=True)
+    busy_charger = BooleanField(required=True)
+    high_price = BooleanField(required=True)
