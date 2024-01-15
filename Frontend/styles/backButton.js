750c26fc06bee55dfc2410364f985c513b58f432
David <D00238516@student.dkit.ie>
2024-01-15 00:14:45 +0000

    💄Small UI Update to FriendsScreen 💄Added Working Back Button

--- Frontend/styles/backButton.js
+++ Frontend/styles/backButton.js
@@ -2,23 +2,26 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { TouchableOpacity, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Container = styled.View`
     position: absolute;
-    top: 0;
+  padding: 10px;
+    left: 0;
     bottom: 0;
 `;
 
 const BackButtonContainer = styled(TouchableOpacity)`
     padding: 10px;
-    background-color: #007bff; // You can customize the color
+    background-color: transparent; // You can customize the color
     border-radius: 5px;
     align-items: center;
 `;
 
-const ButtonText = styled(Text)`
-    color: white;
-    font-size: 16px;
+// Updated styling for the FontAwesome5 icon
+const Icon = styled(FontAwesome5)`
+    color: #515151;
+    font-size: 18px;
 `;
 
 const BackButton = ({ title = 'Back' }) => {
@@ -27,7 +30,7 @@ const BackButton = ({ title = 'Back' }) => {
     return (
         <Container>
             <BackButtonContainer onPress={() => navigation.goBack()}>
-                <ButtonText>{title}</ButtonText>
+               <Icon name="arrow-left" />
             </BackButtonContainer>
         </Container>
 
