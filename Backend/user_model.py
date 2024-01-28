070a4fbf02abf00f058275fe38d1e34628116371
LiamHarmon <d00236258@student.dkit.ie>
2024-01-28 17:02:56 +0000

    :sparkles: User Model connected to DB

--- Backend/user_model.py
+++ Backend/user_model.py
@@ -2,6 +2,8 @@ import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+from models import BudgetHistory, Users
+import database
 
 # Define the load_saved_model function
 def load_saved_model(model_path):
@@ -20,10 +22,6 @@ def make_prediction(model, scaler, last_weeks_data, look_back):
     predicted_price = scaler.inverse_transform(predicted_price)
     return predicted_price[0][0]
 
-# Define the round_to_nearest_10 function
-def round_to_nearest_10(value):
-    return round(value / 10.0) * 10
-
 # Define the model path and look_back period
 look_back = 10
 model_path = 'Backend/updated_user_model.h5'
@@ -37,8 +35,22 @@ df = pd.read_csv('Backend/unseen_spending_data.csv')
 data = df['Total'].values.reshape(-1, 1)
 scaler.fit(data)
 
-# Input data for the prediction
-last_weeks_data = np.array([70,120,100,50.60,80,100,140,150,70,50])
+users = Users.objects(username='ndavido').first()
+user_budget_history = BudgetHistory.objects(user=Users.objects(username='ndavido').first()).order_by('-date_created')
+
+if user_budget_history:
+    for budget_history in user_budget_history:
+        print(budget_history.new_budget)
+else:
+    print('Budget not set')
+
+def round_to_nearest_10(predicted_price):
+    # Round the predicted price to the nearest 10
+    adjusted_predicted_price = round(predicted_price / 10) * 10
+    return adjusted_predicted_price
+
+# Get the last week's data
+last_weeks_data = [budget_history.new_budget for budget_history in user_budget_history][:look_back]
 
 # Make the prediction
 predicted_price = make_prediction(model, scaler, last_weeks_data, look_back)
