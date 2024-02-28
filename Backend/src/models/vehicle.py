5dcf839a523279b12b7a4c2d0a6ab283053c4d43
david <D00239311@student.dkit.ie>
2024-02-28 09:51:37 +0000

    :sparkles: Updated vehicle model with headers from CSV

--- Backend/src/models/vehicle.py
+++ Backend/src/models/vehicle.py
@@ -1,14 +1,27 @@
 #! /usr/bin/env python3
 
-from mongoengine import Document, ReferenceField, IntField, StringField
-from .user import Users
+from mongoengine import Document, IntField, StringField, FloatField, ListField
+
 
 class Vehicle(Document):
-    user = ReferenceField(Users, reverse_delete_rule='CASCADE')
-    year = IntField()
+    id_trim = IntField()
     make = StringField()
     model = StringField()
-    engine_size = StringField()
-    license_plate = StringField()
-    # Include 'Electric' as an option
-    fuel_type = StringField(choices=('Petrol', 'Diesel', 'Electric'))
+    generation = StringField()
+    year_from = IntField()
+    year_to = IntField()
+    series = StringField()
+    trim = StringField()
+    body_type = StringField()
+    engine_type = StringField()
+    turnover_of_maximum_torque_rpm = IntField()
+    capacity_cm3 = IntField()
+    engine_hp = IntField()
+    engine_hp_rpm = IntField()
+    transmission = StringField()
+    mixed_fuel_consumption_per_100_km_l = FloatField()
+    range_km = IntField()
+    emission_standards = StringField()
+    fuel_tank_capacity_l = IntField()
+    CO2_emissions_g_km = IntField()
+    car_class = StringField()
