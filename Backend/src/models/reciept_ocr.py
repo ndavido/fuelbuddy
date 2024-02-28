dece9589c6644cfb7ed9df8caccb5e7650db6fde
ndavido <D00239107@student.dkit.ie>
2024-02-28 10:36:58 +0000

    🚧🗃️ Added missing field

--- Backend/src/models/reciept_ocr.py
+++ Backend/src/models/reciept_ocr.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 
-from mongoengine import Document, BinaryField, DateTimeField, DecimalField, ListField, ReferenceField, EmbeddedDocumentField, EmbeddedDocument
+from mongoengine import Document, BinaryField, DateTimeField, DecimalField, StringField, ReferenceField
 from datetime import datetime
 
 
 class RecieptOcr(Document):
     user = ReferenceField('Users', required=True)
     reciept = BinaryField(required=True)  # image in bit
+    fuel_type = StringField()
     volume = DecimalField(precision=2)
     price_per_litre = DecimalField(precision=3)
     total = DecimalField(precision=2)
