1777dc2b852bc9b8f14c872c07610d1e2061f132
ndavido <ndavido02@gmail.com>
2024-03-01 11:27:32 +0000

    🚧♻️Refactored code structure

--- Backend/src/services/ocr_service.py
+++ Backend/src/services/ocr_service.py
@@ -18,7 +18,6 @@ def upload_receipt():
         return jsonify({'error': 'No selected file'}), 400
     if file and allowed_file(file.filename):
         try:
-            # Read the file's content into a numpy array
             image = retrieve_image(file)
 
             extracted_info_single = extract_receipt_info_single(
@@ -26,7 +25,6 @@ def upload_receipt():
             receipt_image_base64 = convert_image_to_base64(image)
             return jsonify(extracted_info_single, receipt_image_base64)
         except Exception as e:
-            # Handle general exceptions
             return jsonify({'error': 'Failed to process image', 'details': str(e)}), 500
     else:
         return jsonify({'error': 'Invalid file format'}), 400
@@ -36,18 +34,24 @@ def upload_receipt():
 def save_receipt():
     try:
         receipt_data = request.get_json()
-        user = Users.objects.get(username=receipt_data.get('username'))
+
+        username = receipt_data.get('username')
         receipt_image_base64 = receipt_data.get('receipt_image_base64')
         fuel_type = receipt_data.get('fuel_type')
         volume = receipt_data.get('volume')
         price_per_litre = receipt_data.get('price_per_litre')
         total = receipt_data.get('total')
 
-        reciept = ReceiptOcr(user=user, reciept=receipt_image_base64, fuel_type=fuel_type,
-                             volume=volume, price_per_litre=price_per_litre, total=total)
-
-        reciept.save()
+        user = Users.objects.get(username=username)
+        receipt = ReceiptOcr(
+            user=user,
+            receipt=receipt_image_base64,
+            fuel_type=fuel_type,
+            volume=volume,
+            price_per_litre=price_per_litre,
+            total=total
+        )
+        receipt.save()
 
     except Exception as e:
-        # Handle general exceptions
-        return jsonify({'error': 'Failed to process image', 'details': str(e)}), 500
+        return jsonify({'error': 'Failed to save receipt', 'details': str(e)}), 500
