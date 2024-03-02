6de4e0847b218ba6839d3f4920b60046eef2a5c8
ndavido <ndavido02@gmail.com>
2024-03-02 20:56:28 +0000

    🚧♻️Refactored account to use JWT

--- Backend/src/services/account_service.py
+++ Backend/src/services/account_service.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python3
-
+# account_service.py
 from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
 from src.middleware.api_key_middleware import require_api_key
 from src.utils.encryption_utils import aes_decrypt, aes_encrypt, encryption_key
 from src.models.user import Users
@@ -8,99 +8,73 @@ from src.utils.helper_utils import handle_api_error
 
 
 @require_api_key
+@jwt_required()
 def account():
     try:
-        data = request.get_json()
-        encrypted_phone = data.get('id')
-
-        print("Encrypted Phone Number:", encrypted_phone)
-
-        if not encrypted_phone:
-            return jsonify({"error": "Phone number not provided"}), 400
-
-        try:
-            decrypted_phone = aes_decrypt(encrypted_phone, encryption_key)
-            print("Decrypted Phone Number:", decrypted_phone)
-        except Exception as e:
-            return jsonify({"error": f"Error decrypting phone number: {str(e)}"}), 500
-
-        user_info = Users.objects(phone_number=encrypted_phone).first()
+        user_id = get_jwt_identity()
+        user_info = Users.objects(id=user_id).first()
 
         if user_info:
-            # Convert the user_info to a Python dictionary
             user_info_dict = user_info.to_mongo().to_dict()
-
-            # Replace the encrypted phone number with the decrypted one
+            decrypted_phone = aes_decrypt(
+                user_info_dict['phone_number'], encryption_key)
             user_info_dict['phone_number'] = decrypted_phone
 
-            # Exclude some fields from the response if needed
             excluded_fields = ['_id', 'verification_code',
-                               'verified', 'login_code', 'updated_at']
+                               'verified', 'verification_code_sent_at', 'updated_at']
             for field in excluded_fields:
                 user_info_dict.pop(field, None)
 
             return jsonify({"message": "User found", "user": user_info_dict}), 200
         else:
             return jsonify({"error": "User not found"}), 404
-
     except Exception as e:
         return jsonify({"error": f"Error fetching user account information: {str(e)}"}), 500
 
 
 @require_api_key
+@jwt_required()
 def delete_account():
     try:
-        data = request.get_json()
-        encrypted_phone = data.get('id')
-
-        if not encrypted_phone:
-            return jsonify({"error": "Phone number not provided"}), 400
-
-        user_info = Users.objects(phone_number=encrypted_phone).first()
+        user_id = get_jwt_identity()
+        user_info = Users.objects(id=user_id).first()
 
         if user_info:
             user_info.delete()
-            return jsonify({"message": "Account deleted successfully!"})
+            return jsonify({"message": "Account deleted successfully!"}), 200
         else:
             return jsonify({"error": "User not found"}), 404
-
     except Exception as e:
-        return handle_api_error(e)
+        return jsonify({"error": str(e)}), 500
 
 
 @require_api_key
+@jwt_required()
 def logout():
-    try:
-        return jsonify({"message": "Logout successful!"})
-    except Exception as e:
-        return handle_api_error(e)
+    return jsonify({"message": "Logout successful!"}), 200
 
 
-# TODO Fix standardising phone number/ hashing phone number
 @require_api_key
+@jwt_required()
 def edit_account():
     try:
-        data = request.get_json()
-        username = data.get('username')
-
-        user = Users.objects(username=username).first()
+        user_id = get_jwt_identity()
+        user = Users.objects(id=user_id).first()
 
         if not user:
             return jsonify({"error": "User not found"}), 404
 
-        if 'phone_number' in data:
-            decrypted_phone = aes_encrypt(data['phone_number'], encryption_key)
-            user.phone_number = decrypted_phone
+        data = request.get_json()
 
-        # Update user fields if they are in the request
+        if 'phone_number' in data:
+            encrypted_phone = aes_encrypt(data['phone_number'], encryption_key)
+            user.phone_number = encrypted_phone
         if 'full_name' in data:
             user.full_name = data['full_name']
         if 'email' in data:
             user.email = data['email']
 
         user.save()
-
         return jsonify({"message": "Account updated successfully"}), 200
-
     except Exception as e:
-        return handle_api_error(e)
+        return jsonify({"error": str(e)}), 500
