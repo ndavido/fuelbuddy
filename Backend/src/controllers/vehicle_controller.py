7497bb847807440a55742abf6bc401f4778e94c3
david <D00239311@student.dkit.ie>
2024-03-03 18:20:20 +0000

    :bento: Created get_models_for_make & get_years_for_model ✅ Tested GET Request via Postman :bento: Added small script for fixing makes in DB

--- Backend/src/controllers/vehicle_controller.py
+++ Backend/src/controllers/vehicle_controller.py
@@ -1,8 +1,10 @@
-#! /usr/bin/env python3
-
 from flask import Blueprint
-from src.services.vehicle_service import get_vehicle_makes
+from src.services.vehicle_service import get_vehicle_makes, get_models_for_make, get_years_for_model
+
 
 vehicle_blueprint = Blueprint('vehicle', __name__)
 
+# GET vehicle methods
 vehicle_blueprint.route('/vehicle_makes', methods=['GET'])(get_vehicle_makes)
+vehicle_blueprint.route('/vehicle_models/<make>', methods=['GET'])(get_models_for_make)
+vehicle_blueprint.route('/vehicle_years/<model>', methods=['GET'])(get_years_for_model)
--- Backend/src/services/vehicle_service.py
+++ Backend/src/services/vehicle_service.py
@@ -8,6 +8,9 @@ from datetime import datetime
 from mongoengine.queryset.visitor import Q
 from src.utils.helper_utils import handle_api_error
 
+# ref: https://docs.python.org/3/tutorial/datastructures.html
+# ref: https://www.geeksforgeeks.org/python-set-method/
+
 # Get all vehicle makes from the database
 @require_api_key
 def get_vehicle_makes():
@@ -18,3 +21,30 @@ def get_vehicle_makes():
     except Exception as e:
         return handle_api_error(e)
 
+@require_api_key
+def get_models_for_make(make):
+    try:
+        models = set()
+        vehicles = Vehicle.objects(make=make)
+        for vehicle in vehicles:
+            for model_info in vehicle.models:
+                models.add(model_info.model)
+        return jsonify({'models': list(models)})
+    except Exception as e:
+        return handle_api_error(e)
+
+
+# Route to get all years for the selected model
+@require_api_key
+def get_years_for_model(model):
+    try:
+        years = set()
+        vehicles = Vehicle.objects(models__model=model)
+        for vehicle in vehicles:
+            for model_info in vehicle.models:
+                if model_info.model == model:
+                    for year_info in model_info.years:
+                        years.add(year_info.year)
+        return jsonify({'years': list(years)})
+    except Exception as e:
+        return handle_api_error(e)
