858e9f5a213c2bed02be61d6171401fd448f66c8
ndavido <ndavido02@gmail.com>
2024-03-03 21:09:25 +0000

    🚸Disabled Twilio

--- Backend/src/services/auth_service.py
+++ Backend/src/services/auth_service.py
@@ -12,7 +12,6 @@ from src.utils.validation_utils import validate_phone_number, validate_verificat
 from src.utils.encryption_utils import aes_encrypt, encryption_key
 from src.models.user import Users
 from twilio.rest import Client
-
 new_user_session = {}
 
 
@@ -44,7 +43,7 @@ def register():
             "full_name": full_name,
             "username": username,
             "phone_number": encrypted_phone_number,
-            "verification_code": hashed_code,
+            "verification_code": 000000,
             "verification_code_sent_at": now,
             "verified": False,
             "roles": ["user"],
@@ -52,7 +51,7 @@ def register():
             "updated_at": now
         }
 
-        send_text_code(verification_code, full_phone_number)
+        # send_text_code(verification_code, full_phone_number)
 
         return jsonify({"message": "Verification code sent successfully!"})
 
@@ -77,7 +76,9 @@ def verify():
         if datetime.now() - user_data["verification_code_sent_at"] > timedelta(minutes=10):
             return jsonify({"error": "Verification code expired"}), 410
 
-        if bcrypt.checkpw(code.encode('utf-8'), user_data['verification_code']):
+        # ? Commented out for now
+        # if bcrypt.checkpw(code.encode('utf-8'), user_data['verification_code']):
+        if code == user_data['verification_code']:
             new_user = Users(**user_data)
             new_user.save()  # Save the verified user to the database
 
@@ -117,11 +118,13 @@ def login():
                 verification_code.encode('utf-8'), bcrypt.gensalt())
             now = datetime.now()
 
-            # Update the user with the new login code and the time it was sent
-            user.update(set__verification_code=hashed_verification_code.decode(
-                'utf-8'), set__verification_code_sent_at=now)
+            # ? Commented out for now
+            # user.update(set__verification_code=hashed_verification_code.decode(
+            #     'utf-8'), set__verification_code_sent_at=now)
+            user.update(set__verification_code=000000,
+                        set__verification_code_sent_at=now)
 
-            send_text_code(verification_code, standardized_phone_number)
+            # send_text_code(verification_code, standardized_phone_number)
             return jsonify({"message": "Login code sent successfully!"})
 
         else:
@@ -151,7 +154,9 @@ def login_verify():
 
         # Check if the login code matches and hasn't expired (assuming a 10-minute expiry)
         if "verification_code" in user and "verification_code_sent_at" in user and datetime.now() - user.verification_code_sent_at < timedelta(minutes=10):
-            if bcrypt.checkpw(code.encode('utf-8'), user.verification_code.encode('utf-8')):
+            # ? Commented out for now
+            # if bcrypt.checkpw(code.encode('utf-8'), user.verification_code.encode('utf-8')):
+            if code == user.verification_code:
                 # Clear the login code to prevent reuse
                 user.update(unset__verification_code=1,
                             unset__verification_code_sent_at=1)
