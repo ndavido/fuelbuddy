70517b0a40b403762c9923bc6716b7e662603ac3
DavidCampion <campiondavid02@gmail.com>
2024-03-11 19:09:17 +0000

    ✨Started OCR Implementation to Frontend

--- Frontend/Screens/ScanScreen.js
+++ Frontend/Screens/ScanScreen.js
@@ -1,8 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, TextInput, FlatList, TouchableOpacity, Button, StyleSheet, Modal} from 'react-native';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useNavigation} from '@react-navigation/native';
+import React, {useState, useEffect, useRef} from 'react';
+import {View, Text, Button, StyleSheet, Modal, Image} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import {Camera, CameraType} from 'expo-camera';
 
 import MainLogo from '../styles/mainLogo';
 import {AccountContainer, Content, InputTxt, Main, TextWrapper, Wrapper} from '../styles/styles';
@@ -12,23 +11,117 @@ import {H3, H5, H6} from "../styles/text";
 const apiKey = process.env.REACT_NATIVE_API_KEY;
 
 const ScanScreen = () => {
-    const [loading, setLoading] = useState(true);
+    const [imageUri, setImageUri] = useState(null);
+    const [type, setType] = useState(CameraType.back);
+    const [cameraModalVisible, setCameraModalVisible] = useState(false);
 
+    const cameraRef = useRef(null);
+
+    const requestPermissions = async () => {
+        const {cameraStatus} = await Camera.requestCameraPermissionsAsync();
+        const {mediaLibraryStatus} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        return cameraStatus === 'granted' && mediaLibraryStatus === 'granted';
+    };
+
+    function toggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    }
+
+    useEffect(() => {
+        const hasPermissions = requestPermissions();
+        if (!hasPermissions) {
+            console.error('Permissions not granted');
+        }
+    }, []);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            console.log('imageUri:', result.assets[0].uri);
+            setImageUri(result.assets[0].uri);
+        }
+    };
+
+    const takePicture = async () => {
+        if (cameraRef.current) {
+            let result = await cameraRef.current.takePictureAsync({
+                quality: 0.5,
+            });
+
+            if (!result.cancelled) {
+                setImageUri(result.uri);
+                setCameraModalVisible(false);
+            }
+        }
+    };
+
+    const sendImageToBackend = async () => {
+        if (!imageUri) {
+            return;
+        }
+        try {
+            console.log('Uploading image:', imageUri);
+
+        } catch (error) {
+            console.error('Error uploading image:', error);
+        }
+    };
 
     return (
         <Main>
             <MainLogo/>
             <Wrapper>
-                <AccountContainer>
+                <AccountContainer style={{minHeight: 800}}>
                     <Content>
-                        <TextWrapper>
-                            <H5 tmargin='10px' bmargin='10px'>Placeholder, Feature TBC</H5>
-                        </TextWrapper>
+                        {/*<Button title="Pick Image" onPress={pickImage}/>*/}
+                        <Button title="Take Picture" onPress={() => setCameraModalVisible(true)}/>
+                        <Button title="Confirm Image" onPress={sendImageToBackend} disabled={!imageUri}/>
+                        {imageUri && <Image source={{uri: imageUri}} style={{width: 200, height: 200}}/>}
                     </Content>
                 </AccountContainer>
             </Wrapper>
+
+            <Modal visible={cameraModalVisible} onRequestClose={() => setCameraModalVisible(false)}>
+                <View style={styles.cameraContainer}>
+                    <Camera
+                        style={styles.camera}
+                        type={type}
+                        ref={cameraRef}
+                        onCameraReady={() => console.log('Camera ready')}
+                    >
+                        <View style={styles.buttonContainer}>
+                            <Button title="Take Picture" onPress={takePicture}/>
+                            <Button title="Exit" onPress={() => setCameraModalVisible(false)}/>
+                        </View>
+                    </Camera>
+                </View>
+            </Modal>
         </Main>
     );
 };
+
+const styles = StyleSheet.create({
+    cameraContainer: {
+        flex: 1,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'flex-end',
+        marginBottom: 50,
+    },
+});
 
 export default ScanScreen;
