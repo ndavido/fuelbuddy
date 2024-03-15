0238358d2a4799ed332cb90e3b01175dbf6a0731
ndavido <ndavido02@gmail.com>
2024-03-15 17:56:33 +0000

    🚧:bug: updated decode_base64_image

--- Backend/src/services/ocr_service.py
+++ Backend/src/services/ocr_service.py
@@ -7,27 +7,36 @@ from src.utils.ocr_utils import extract_receipt_info_single, allowed_file, ocr_c
 from src.utils.image_utils import convert_image_to_base64, decode_base64_image
 from src.models.receipt_ocr import ReceiptOcr
 from src.models.user import Users
+import cv2
+import numpy as np
+import base64
 
 
 @require_api_key
 @jwt_required()
 def upload_receipt():
-
     data = request.get_json()
     image_data_base64 = data.get('image')
     if not image_data_base64:
         return jsonify({'error': 'No image data provided'}), 400
 
+    # Decoding the base64 string to bytes
     image_data = decode_base64_image(image_data_base64)
-
     if image_data is None:
         return jsonify({'error': 'Invalid or missing image data'}), 400
 
-    try:
-        extracted_info_single = extract_receipt_info_single(
-            ocr_cleanup(image_data), image_data)
+    # Convert the bytes data to a numpy array and then to an OpenCV image
+    nparr = np.frombuffer(image_data, np.uint8)
+    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        receipt_image_base64 = convert_image_to_base64(image_data)
+    try:
+        # Assuming ocr_cleanup and extract_receipt_info_single can work directly with the OpenCV image
+        extracted_info_single = extract_receipt_info_single(
+            ocr_cleanup(img_np), img_np)
+
+        # Re-encode the OpenCV image to base64 for the response if necessary
+        _, buffer = cv2.imencode('.jpg', img_np)
+        receipt_image_base64 = base64.b64encode(buffer).decode('utf-8')
 
         return jsonify({
             'extracted_info': extracted_info_single,
