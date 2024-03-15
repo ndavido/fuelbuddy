07fa8138bdf062ead16e774803c7dd55b4f6b56d
ndavido <ndavido02@gmail.com>
2024-03-15 17:30:08 +0000

    🚧⚡️Have upload reciept take image as base64

--- Backend/src/services/ocr_service.py
+++ Backend/src/services/ocr_service.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from src.middleware.api_key_middleware import require_api_key
 from src.utils.ocr_utils import extract_receipt_info_single, allowed_file, ocr_cleanup
-from src.utils.image_utils import convert_image_to_base64, retrieve_image
+from src.utils.image_utils import convert_image_to_base64, decode_base64_image
 from src.models.receipt_ocr import ReceiptOcr
 from src.models.user import Users
 
@@ -13,23 +13,29 @@ from src.models.user import Users
 @jwt_required()
 def upload_receipt():
 
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-    if file and allowed_file(file.filename):
-        try:
-            image = retrieve_image(file)
+    data = request.get_json()
+    image_data_base64 = data.get('image')
+    if not image_data_base64:
+        return jsonify({'error': 'No image data provided'}), 400
 
-            extracted_info_single = extract_receipt_info_single(
-                ocr_cleanup(image), image)
-            receipt_image_base64 = convert_image_to_base64(image)
-            return jsonify(extracted_info_single, receipt_image_base64)
-        except Exception as e:
-            return jsonify({'error': 'Failed to process image', 'details': str(e)}), 500
-    else:
-        return jsonify({'error': 'Invalid file format'}), 400
+    image_data = decode_base64_image(image_data_base64)
+
+    if image_data is None:
+        return jsonify({'error': 'Invalid or missing image data'}), 400
+
+    try:
+        extracted_info_single = extract_receipt_info_single(
+            ocr_cleanup(image_data), image_data)
+
+        receipt_image_base64 = convert_image_to_base64(image_data)
+
+        return jsonify({
+            'extracted_info': extracted_info_single,
+            'receipt_image_base64': receipt_image_base64
+        })
+
+    except Exception as e:
+        return jsonify({'error': 'Failed to process image', 'details': str(e)}), 500
 
 
 @require_api_key
--- Backend/src/utils/image_utils.py
+++ Backend/src/utils/image_utils.py
@@ -10,8 +10,18 @@ def convert_image_to_base64(image_data):
     return base64.b64encode(image_data).decode('utf-8')
 
 
+def decode_base64_image(image_data):
+    if image_data:
+        if 'data:image/jpeg;base64,' in image_data:
+            header, image_data = image_data.split(',', 1)
+        return base64.b64decode(image_data)
+    else:
+        return None
+
+
 def retrieve_image(file):
     in_memory_file = io.BytesIO()
     file.save(in_memory_file)
     data = np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
     image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    return image
