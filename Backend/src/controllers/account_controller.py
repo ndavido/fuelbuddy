6850fa0a41dd1ef38f817f7155769dfc18f5e90f
ndavido <ndavido02@gmail.com>
2024-03-22 21:33:07 +0000

    :sparkles: Created upload_profile_picture function

--- Backend/src/controllers/account_controller.py
+++ Backend/src/controllers/account_controller.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from flask import Blueprint
-from src.services.account_service import account, delete_account, edit_account, logout
+from src.services.account_service import account, delete_account, edit_account, logout, upload_profile_picture
 
 account_blueprint = Blueprint('account', __name__)
 
@@ -9,3 +9,5 @@ account_blueprint.route('/account', methods=['POST'])(account)
 account_blueprint.route('/delete_account', methods=['POST'])(delete_account)
 account_blueprint.route('/edit_account', methods=['PATCH'])(edit_account)
 account_blueprint.route('/logout', methods=['POST'])(logout)
+account_blueprint.route('/upload_profile_picture',
+                        methods=['POST'])(upload_profile_picture)
--- Backend/src/utils/image_utils.py
+++ Backend/src/utils/image_utils.py
@@ -4,6 +4,7 @@ import base64
 import io
 import cv2
 import numpy as np
+from flask import jsonify
 
 
 def convert_image_to_base64(image_data):
@@ -35,4 +36,19 @@ def retrieve_image(file):
     file.save(in_memory_file)
     data = np.frombuffer(in_memory_file.getvalue(), dtype=np.uint8)
     image = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    return image
+
+
+def upload_image(image):
+    if not image:
+        return jsonify({'error': 'No image data provided'}), 400
+
+    image_data_bytes = decode_base64_image(image)  # Decode from Base64
+
+    image = cv2.imdecode(np.frombuffer(
+        image_data_bytes, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+    if image is None:
+        return jsonify({'error': 'Invalid or missing image data'}), 400
+
     return image
