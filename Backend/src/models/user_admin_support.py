33d8a2878ee13b0adbd3def71151ea303d91514b
ndavido <ndavido02@gmail.com>
2024-03-22 22:57:51 +0000

    :sparkles: Add user admin support endpoints and functionality

--- Backend/src/models/user_admin_support.py
+++ Backend/src/models/user_admin_support.py
@@ -1,3 +1,32 @@
 #! /usr/bin/env python3
 
-from mongoengine import Document, FloatField
+from mongoengine import Document, EmbeddedDocument, EmbeddedDocumentListField, ReferenceField, StringField, DateTimeField, BooleanField
+from datetime import datetime
+from mongoengine.queryset.visitor import Q
+from .user import Users
+
+
+class Message(EmbeddedDocument):
+    sender = StringField(required=True, choices=(
+        'user', 'admin'))  # Restrict to 'user' or 'admin'
+    content = StringField(required=True)
+    timestamp = DateTimeField(default=datetime.utcnow)
+
+
+class SupportTicket(Document):
+    user = ReferenceField(Users, reverse_delete_rule='CASCADE', required=True)
+    messages = EmbeddedDocumentListField(Message)
+    # Track if the ticket is open or closed
+    is_open = BooleanField(default=True)
+    # User confirmation if the issue is solved
+    is_solved = BooleanField(default=False)
+    # Keep track of the last update
+    last_updated = DateTimeField(default=datetime.utcnow)
+
+    meta = {
+        'indexes': [
+            'user',
+            'is_open',
+            {'fields': ['-last_updated'], 'background': True}
+        ]
+    }
--- Backend/src/services/user_admin_support_service.py
+++ Backend/src/services/user_admin_support_service.py
@@ -2,9 +2,11 @@
 
 from flask import request, jsonify
 from src.models import Users
+from src.models.user_admin_support import SupportTicket, Message
 from src.middleware.api_key_middleware import require_api_key
 from src.utils.helper_utils import handle_api_error
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from datetime import datetime
 
 
 @require_api_key
@@ -74,6 +76,118 @@ def remove_role():
             user.save()
 
         return jsonify({"message": "Role removed successfully"}), 200
+
+    except Exception as e:
+        return handle_api_error(e)
+
+
+@require_api_key
+@jwt_required()
+def send_support_ticket():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+    message = data["message"]
+    try:
+        user = Users.objects(id=user_id).first()
+
+        ticket = SupportTicket(user=user)
+        ticket.messages.append(Message(sender="user", content=message))
+        ticket.save()
+
+        return jsonify({"message": "Support ticket sent successfully"}), 200
+
+    except Exception as e:
+        return handle_api_error(e)
+
+
+@require_api_key
+@jwt_required()
+def view_support_tickets():
+    user_id = get_jwt_identity()
+    try:
+        user = Users.objects(id=user_id).first()
+
+        if "admin" in user.roles or "Developer" in user.roles:
+            tickets = SupportTicket.objects(is_open=True)
+            tickets_data = [ticket.to_json() for ticket in tickets]
+        else:
+            tickets = SupportTicket.objects(
+                is_open=True, user=user).order_by("-last_updated")
+            tickets_data = [{
+                'id': str(ticket.id),
+                'messages': [{'sender': msg.sender, 'content': msg.content, 'timestamp': msg.timestamp} for msg in ticket.messages],
+                'is_open': ticket.is_open,
+                'is_solved': ticket.is_solved,
+                'last_updated': ticket.last_updated
+            } for ticket in tickets]
+
+        return jsonify({"tickets": tickets_data}), 200
+
+    except Exception as e:
+        return handle_api_error(e)
+
+
+@require_api_key
+@jwt_required()
+def reply_to_ticket():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+    ticket_id = data["ticket_id"]
+    message = data["message"]
+    try:
+        user = Users.objects(id=user_id).first()
+        if "admin" in user.roles or "Developer" in user.roles:
+            ticket = SupportTicket.objects(id=ticket_id).first()
+            ticket.messages.append(Message(sender="admin", content=message))
+            ticket.last_updated = datetime.utcnow()
+            ticket.save()
+        else:
+            ticket.messages.append(Message(sender="user", content=message))
+            ticket.last_updated = datetime.utcnow()
+            ticket.save()
+
+        return jsonify({"message": "Reply sent successfully"}), 200
+
+    except Exception as e:
+        return handle_api_error(e)
+
+#! Function for Admins ONLY!!!
+
+
+@require_api_key
+@jwt_required()
+def close_support_ticket():
+    user_id = get_jwt_identity()
+    data = request.get_json()
+    ticket_id = data["ticket_id"]
+    try:
+        user = Users.objects(id=user_id).first()
+        if "admin" not in user.roles or "Developer" not in user.roles:
+            return jsonify({"error": "Unauthorized access"}), 401
+
+        ticket = SupportTicket.objects(id=ticket_id).first()
+        ticket.is_open = False
+        ticket.last_updated = datetime.utcnow()
+        ticket.save()
+
+        return jsonify({"message": "Ticket closed successfully"}), 200
+
+    except Exception as e:
+        return handle_api_error(e)
+
+
+@require_api_key
+@jwt_required()
+def mark_ticket_as_solved():
+    data = request.get_json()
+    ticket_id = data["ticket_id"]
+    try:
+        ticket = SupportTicket.objects(id=ticket_id).first()
+        ticket.is_solved = True
+        ticket.last_updated = datetime.utcnow()
+        ticket.save()
+
+        return jsonify({"message": "Ticket marked as solved"}), 200
 
     except Exception as e:
         return handle_api_error(e)
