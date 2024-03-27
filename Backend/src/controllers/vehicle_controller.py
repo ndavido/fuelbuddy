9c24d23e7204e20df93c9fd9fea0fcc271757a85
david <D00239311@student.dkit.ie>
2024-03-27 20:18:02 +0000

    ✨ Refactored vehicle route names

--- Backend/src/controllers/vehicle_controller.py
+++ Backend/src/controllers/vehicle_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from src.services.vehicle_service import (get_vehicle_makes, get_models_for_make, get_years_for_model,
+from src.services.vehicle_service import (get_vehicle_makes, get_vehicle_models_for_makes, get_vehicle_years_for_model,
                                           create_user_vehicle, get_user_vehicle, delete_user_vehicle, update_user_vehicle)
 
 
@@ -7,9 +7,8 @@ vehicle_blueprint = Blueprint('vehicle', __name__)
 
 # GET vehicle methods
 vehicle_blueprint.route('/vehicle_makes', methods=['GET'])(get_vehicle_makes)
-vehicle_blueprint.route('/vehicle_models/<make>', methods=['GET'])(get_models_for_make)
-vehicle_blueprint.route('/vehicle_years/<model>', methods=['GET'])(get_years_for_model)
-
+vehicle_blueprint.route('/vehicle_models/<make>', methods=['GET'])(get_vehicle_models_for_makes)
+vehicle_blueprint.route('/vehicle_years/<model>', methods=['GET'])(get_vehicle_years_for_model)
 
 # GET User Vehicle method
 vehicle_blueprint.route('/get_user_vehicle/', methods=['GET'])(get_user_vehicle)
--- Backend/src/services/vehicle_service.py
+++ Backend/src/services/vehicle_service.py
@@ -136,6 +136,7 @@ def delete_user_vehicle():
         return jsonify({'error': 'Failed to delete vehicle', 'details': str(e)}), 500
 
 # General Vehicle Routes for 'vehicles_data' collection
+
 @require_api_key
 @jwt_required()
 def get_vehicle_makes():
@@ -143,11 +144,12 @@ def get_vehicle_makes():
         makes = Vehicle.objects.distinct('make')
         return jsonify({'makes': makes}), 200
     except Exception as e:
-        return jsonify({'error': 'Failed to retrieve vehicle makes', 'details': str(e)}), 500
+        return jsonify({'error': 'Failed to retrieve all vehicle makes', 'details': str(e)}), 500
+
 
 @require_api_key
 @jwt_required()
-def get_models_for_make(make):
+def get_vehicle_models_for_makes(make):
     try:
         models = Vehicle.objects(make=make).distinct('models.model')
         return jsonify({'make': make, 'models': models}), 200
@@ -157,10 +159,11 @@ def get_models_for_make(make):
 # Route to get all years for the selected model
 @require_api_key
 @jwt_required()
-def get_years_for_model(model):
+def get_vehicle_years_for_model(model):
     try:
         trim_info_by_year = get_trim_info_by_year(model)
         return jsonify(trim_info_by_year), 200
     except Exception as e:
         return jsonify({'error': 'Failed to retrieve years for the model', 'details': str(e)}), 500
+
 
