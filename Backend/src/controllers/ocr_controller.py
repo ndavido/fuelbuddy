5433e011c60fbc51d3c048165a65e9d801ea5eef
ndavido <ndavido02@gmail.com>
2024-04-07 13:48:38 +0100

    ✏️ Update past receipt url

--- Backend/src/controllers/ocr_controller.py
+++ Backend/src/controllers/ocr_controller.py
@@ -8,5 +8,5 @@ ocr_blueprint = Blueprint('ocr', __name__)
 ocr_blueprint.route('/ocr_reciept_image_upload',
                     methods=['POST'])(upload_receipt)
 ocr_blueprint.route('/ocr_reciept_image_save', methods=['POST'])(save_receipt)
-ocr_blueprint.route('/ocr_reciept_image_get_past',
+ocr_blueprint.route('/retrieve_past_reciepts',
                     methods=['GET'])(get_past_reciepts)
